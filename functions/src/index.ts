8042905886b15e52e2d3046018fa2f23f2ee28f2
Kwong Tung Nan <tungnan5636@gmail.com>
2024-10-18 00:49:03 +0800

    func: Add functions to add user claims
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 import { FieldValue, getFirestore } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
+import { onDocumentUpdated } from "firebase-functions/v2/firestore";
 
-initializeApp()
-
+initializeApp();
 
 interface UserFirestoreInitialData {
     // TODO: To decide what to record, as we are privacy oriented
@@ -14,8 +15,9 @@ interface UserFirestoreInitialData {
     createdAt: FieldValue;
 }
 
-export const createUserFirestoreEntry = functions.auth.user().onCreate(
-    async (user) => {
+export const createUserFirestoreEntry = functions.auth
+    .user()
+    .onCreate(async (user) => {
         const uid = user.uid;
         const userFirestoreData: UserFirestoreInitialData = {
             uid,
@@ -24,6 +26,35 @@ export const createUserFirestoreEntry = functions.auth.user().onCreate(
             createdAt: FieldValue.serverTimestamp(),
         };
 
-        await getFirestore().collection('users').doc(uid).set(userFirestoreData);
+        await getFirestore()
+            .collection("users")
+            .doc(uid)
+            .set(userFirestoreData);
+    });
+
+export const updateAdminToken = onDocumentUpdated(
+    "users/{docId}",
+    async (event) => {
+        if (!event.data) {
+            return;
+        }
+
+        const newData = event.data.after;
+
+        return getAuth()
+            .setCustomUserClaims(newData.id, newData.get("claims") ?? {})
+            .then(() => {
+                getAuth()
+                    .listUsers()
+                    .then((data) => {
+                        data.users
+                            .filter((val) => {
+                                return val.uid === newData.id;
+                            })
+                            .forEach((val) => {
+                                console.log(val.uid, val.customClaims);
+                            });
+                    });
+            });
     }
 );
