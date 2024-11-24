eaa280d15b5e825c85da901159608e6719462834
Kwong Tung Nan <tungnan5636@gmail.com>
2024-11-24 16:58:02 +0800

    web: Add navigation indicator
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,10 +5,20 @@ import { NzIconModule } from "ng-zorro-antd/icon";
 import { NzLayoutModule } from "ng-zorro-antd/layout";
 import { NzMenuModule } from "ng-zorro-antd/menu";
 import { NzNotificationModule } from "ng-zorro-antd/notification";
+import { filter, tap } from "rxjs";
 
 import { CommonModule, DOCUMENT } from "@angular/common";
 import { Component, Inject, OnInit } from "@angular/core";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import {
+    NavigationCancel,
+    NavigationEnd,
+    NavigationSkipped,
+    NavigationStart,
+    Router,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+} from "@angular/router";
 
 import { AuthService } from "./services/auth.service";
 
@@ -37,18 +47,53 @@ export class AppComponent implements OnInit {
             title: "Organizers",
             href: "/organizers",
             icon: "team",
+            loading: false,
         },
         {
             title: "Events",
             href: "/events",
             icon: "calendar",
+            loading: false,
         },
     ];
 
     constructor(
         public authService: AuthService,
+        private router: Router,
         @Inject(DOCUMENT) private document: Document
     ) {}
 
-    ngOnInit(): void {}
+    getMapIndex(path: string | undefined): number {
+        return this.headerMap.findIndex((val) => {
+            return path?.startsWith(val.href);
+        });
+    }
+
+    ngOnInit(): void {
+        this.router.events
+            .pipe(
+                filter((event) => {
+                    return (
+                        event instanceof NavigationStart ||
+                        event instanceof NavigationEnd ||
+                        event instanceof NavigationCancel ||
+                        event instanceof NavigationSkipped
+                    );
+                }),
+                tap((event) => {
+                    if (event instanceof NavigationStart) {
+                        this.headerMap[this.getMapIndex(event.url)].loading =
+                            true;
+                    } else if (
+                        event instanceof NavigationEnd ||
+                        event instanceof NavigationCancel ||
+                        event instanceof NavigationSkipped
+                    ) {
+                        this.headerMap[this.getMapIndex(event.url)].loading =
+                            false;
+                    }
+                })
+            )
+            .subscribe();
+    }
 }
