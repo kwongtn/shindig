e35f6674c7623d10230cd80425d97d5a06b06e39
Kwong Tung Nan <tungnan5636@gmail.com>
2025-02-18 20:51:30 +0800

    web: Add loading indicator
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/src/app/ui/extract-webpage-bar/extract-webpage-bar.component.ts
+++ app/src/app/ui/extract-webpage-bar/extract-webpage-bar.component.ts
@@ -1,6 +1,7 @@
 import { NzAlertModule } from "ng-zorro-antd/alert";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzInputModule } from "ng-zorro-antd/input";
+import { NzSpinModule } from "ng-zorro-antd/spin";
 
 import { Component, EventEmitter, inject, Output } from "@angular/core";
 import { Functions, httpsCallable } from "@angular/fire/functions";
@@ -9,20 +10,35 @@ import { FormsModule } from "@angular/forms";
 @Component({
     selector: "ui-extract-webpage-bar",
     standalone: true,
-    imports: [NzAlertModule, NzButtonModule, NzInputModule, FormsModule],
+    imports: [
+        NzAlertModule,
+        NzButtonModule,
+        NzInputModule,
+        NzSpinModule,
+        FormsModule,
+    ],
     templateUrl: "./extract-webpage-bar.component.html",
     styleUrl: "./extract-webpage-bar.component.less",
 })
 export class ExtractWebpageBarComponent {
-    @Output() onExtract = new EventEmitter<object>();
-    url: string = "";
     private functions = inject(Functions);
 
+    @Output() onExtract = new EventEmitter<object>();
+
+    url: string = "";
+    isLoading: boolean = false;
+
     runExtract() {
+        this.isLoading = true;
+
         const scrapeWebpage = httpsCallable(this.functions, "scrapeWebpage");
-        scrapeWebpage({ url: this.url }).then((result) => {
-            console.log(result.data);
-            this.onExtract.emit(result.data as object);
-        });
+        scrapeWebpage({ url: this.url })
+            .then((result) => {
+                console.log(result.data);
+                this.onExtract.emit(result.data as object);
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
     }
 }
