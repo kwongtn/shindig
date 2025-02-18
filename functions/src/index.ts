6e8c0c1a73840db4959c6a34f7b1446049fa1802
Kwong Tung Nan <tungnan5636@gmail.com>
2025-02-18 18:02:21 +0800

    func: Add gemini to extract details
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
+import axios from "axios";
 import { initializeApp } from "firebase-admin/app";
 import { getAuth, UserRecord } from "firebase-admin/auth";
 import { FieldValue, getFirestore } from "firebase-admin/firestore";
-import * as functions from "firebase-functions";
+import { defineSecret } from "firebase-functions/params";
+import { auth } from "firebase-functions/v1";
 import { onDocumentUpdated } from "firebase-functions/v2/firestore";
+import { onRequest } from "firebase-functions/v2/https";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const GEMINI_API_KEY = defineSecret("GEMINI_API_KEY");
 
 initializeApp();
 
@@ -15,7 +22,7 @@ interface UserFirestoreInitialData {
     createdAt: FieldValue;
 }
 
-export const createUserFirestoreEntry = functions.auth
+export const createUserFirestoreEntry = auth
     .user()
     .onCreate(async (user: UserRecord) => {
         const uid = user.uid;
@@ -56,5 +63,69 @@ export const updateAdminToken = onDocumentUpdated(
                             });
                     });
             });
+    }
+);
+
+const MODEL_NAME = "gemini-2.0-flash-001";
+
+export const scrapeWebpage = onRequest(
+    {
+        secrets: ["GEMINI_API_KEY"],
+    },
+    async (req, res) => {
+        const url = req.query.url as string;
+
+        if (!url) {
+            res.status(400).send("Please provide a URL as a query parameter.");
+            return;
+        }
+
+        try {
+            const response = await axios.get(url);
+            const html = response.data;
+
+            const genAI = new GoogleGenerativeAI(GEMINI_API_KEY.value());
+            const model = genAI.getGenerativeModel({
+                model: MODEL_NAME,
+                generationConfig: {
+                    responseMimeType: "application/json",
+                },
+            });
+
+            const prompt = `Extract content such that we know what the event is about. Do not change any words. Convert relevant content into markdown for display in webpages.
+                Use the following format:
+                {
+                    "title": string,
+                    "startTime": Date,
+                    "endTime": Date,
+                    "description": string,
+                }
+                `;
+
+            const result = await model.generateContent(
+                `${prompt}\n---\n${html}`
+            );
+            if (
+                !result.response.candidates ||
+                result.response.candidates.length === 0
+            ) {
+                res.status(500).send("No candidates returned from Gemini.");
+                return;
+            }
+
+            try {
+                const text =
+                    result.response.candidates[0].content.parts[0].text;
+                res.status(200).json(JSON.parse(text ?? "")[0]);
+            } catch (error) {
+                console.error("Response not in expected format: ", error);
+                res.status(500).send(
+                    `Response not in expected format: ${error}`
+                );
+            }
+        } catch (error: any) {
+            console.error(error);
+            res.status(500).send(`Error scraping ${url}: ${error}`);
+        }
     }
 );
