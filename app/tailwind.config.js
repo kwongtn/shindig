fce9bdd7e15e9408dca7cd1d68c6d854f49c2f0b
Kwong Tung Nan <tungnan5636@gmail.com>
2025-06-30 13:18:28 +0800

    Attempt to fix prod build
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
   theme: {
     extend: {},
   },
