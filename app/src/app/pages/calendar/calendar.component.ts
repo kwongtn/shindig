c0fdf8f9b1b899d2d5c914e82411cf2529e33862
Kwong Tung Nan <tungnan5636@gmail.com>
2025-07-03 00:48:40 +0800

    feat: Add calendar year view selection
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/src/app/pages/calendar/calendar.component.ts
+++ app/src/app/pages/calendar/calendar.component.ts
@@ -1,13 +1,3 @@
-import { NzAlertModule } from "ng-zorro-antd/alert";
-import { NzBadgeModule } from "ng-zorro-antd/badge";
-import { NzCalendarModule } from "ng-zorro-antd/calendar";
-import { NzDrawerModule } from "ng-zorro-antd/drawer";
-import { NzEmptyModule } from "ng-zorro-antd/empty";
-import { NzGridModule } from "ng-zorro-antd/grid";
-import { NzIconModule } from "ng-zorro-antd/icon";
-import { NzSpinModule } from "ng-zorro-antd/spin";
-import { firstValueFrom, Subscription } from "rxjs";
-import { NzDropDownModule } from "ng-zorro-antd/dropdown";
 import { CommonModule, DOCUMENT } from "@angular/common";
 import {
     Component,
@@ -27,14 +17,21 @@ import {
 } from "@angular/fire/firestore";
 import { FormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import { NzAlertModule } from "ng-zorro-antd/alert";
+import { NzBadgeModule } from "ng-zorro-antd/badge";
+import { NzCalendarModule } from "ng-zorro-antd/calendar";
+import { NzDrawerModule } from "ng-zorro-antd/drawer";
+import { NzEmptyModule } from "ng-zorro-antd/empty";
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzSpinModule } from "ng-zorro-antd/spin";
+import { firstValueFrom, Subscription } from "rxjs";
 
-import { NzButtonModule } from "ng-zorro-antd/button";
-import { environment } from "../../../environments/environment";
 import { AuthService } from "../../services/auth.service";
 import { IEvent } from "../../types";
+import { CopyCalendarUrlButtonComponent } from "../../ui/copy-calendar-url-button/copy-calendar-url-button.component";
 import { EventCardComponent } from "../../ui/event-card/event-card.component";
 import { getCurrentLocalDate } from "../../utils";
-import { CopyCalendarUrlButtonComponent } from "../../ui/copy-calendar-url-button/copy-calendar-url-button.component";
 
 @Component({
     selector: "app-calendar",
@@ -63,6 +60,9 @@ export class CalendarComponent implements OnInit, OnDestroy {
     selectedDate: Date = new Date();
     prevSelectedDate: Date = new Date();
 
+    nzMode: "month" | "year" = "month";
+    prevNzMode: "month" | "year" = "month";
+
     eventCollectionRef = collection(this.firestore, "events");
 
     showLoading = false;
@@ -74,7 +74,6 @@ export class CalendarComponent implements OnInit, OnDestroy {
 
     width: string = "700px";
     isSmallScreen: boolean = false;
-
 
     @HostListener("window:resize")
     resize(): void {
@@ -95,48 +94,103 @@ export class CalendarComponent implements OnInit, OnDestroy {
             this.runQuery();
         });
         firstValueFrom(this.route.paramMap).then((params) => {
-            this.selectedDate = new Date(
-                params.get("date") ?? getCurrentLocalDate()
-            );
+            const dateParam = params.get("date");
+            const splitDate = dateParam?.split("-").map(Number);
+
+            if (splitDate && splitDate.length === 3) {
+                this.selectedDate = new Date(
+                    splitDate[0],
+                    splitDate[1] - 1,
+                    splitDate[2]
+                );
+                this.nzMode = "month";
+            } else if (splitDate && splitDate.length === 2) {
+                this.selectedDate = new Date(splitDate[0], splitDate[1] - 1);
+                this.nzMode = "year";
+            } else {
+                this.selectedDate = getCurrentLocalDate().toDate();
+                this.nzMode = "month";
+            }
+
+            this.runQuery();
+
             this.prevSelectedDate = this.selectedDate;
+            this.prevNzMode = this.nzMode;
         });
     }
 
     runQuery() {
         this.showLoading = true;
 
-        // Gets anything after 14th of last month, and before 14th of next month
-        const queryList: QueryFilterConstraint[] = [
-            where(
-                "startDatetime",
-                ">=",
-                new Date(
-                    this.selectedDate.getFullYear(),
-                    this.selectedDate.getMonth(),
-                    -14
-                )
-            ),
-            where(
-                "startDatetime",
-                "<=",
-                new Date(
-                    this.selectedDate.getFullYear(),
-                    this.selectedDate.getMonth() + 1,
-                    14
-                )
-            ),
-            where("isApproved", "==", true),
-        ];
+        const queryList: QueryFilterConstraint[] = [];
 
-        getDocs(query(this.eventCollectionRef, and(...queryList)))
+        if (this.nzMode === "year") {
+            queryList.push(
+                where(
+                    "startDatetime",
+                    ">=",
+                    new Date(this.selectedDate.getFullYear(), 0, 1)
+                ),
+                where(
+                    "startDatetime",
+                    "<",
+                    new Date(this.selectedDate.getFullYear() + 1, 0, 1)
+                )
+            );
+        } else {
+            // Gets anything after 14th of last month, and before 14th of next month
+            queryList.push(
+                where(
+                    "startDatetime",
+                    ">=",
+                    new Date(
+                        this.selectedDate.getFullYear(),
+                        this.selectedDate.getMonth(),
+                        -14
+                    )
+                ),
+                where(
+                    "startDatetime",
+                    "<=",
+                    new Date(
+                        this.selectedDate.getFullYear(),
+                        this.selectedDate.getMonth() + 1,
+                        14
+                    )
+                )
+            );
+        }
+
+        getDocs(
+            query(
+                this.eventCollectionRef,
+                and(...[...queryList, where("isApproved", "==", true)])
+            )
+        )
             .then((data) => {
                 this.events = {};
                 data.forEach((event) => {
                     const eventData = event.data() as IEvent;
-                    const dateString = eventData.startDatetime
+                    const splitDate = eventData.startDatetime
                         .toDate()
                         .toISOString()
-                        .split("T")[0];
+                        .split("T")[0]
+                        .split("-")
+                        .map(Number);
+
+                    var dateString: string;
+                    if (this.nzMode === "year") {
+                        dateString = `${splitDate[0]}-${splitDate[1]
+                            .toString()
+                            .padStart(2, "0")}`;
+                    } else {
+                        dateString = `${splitDate[0]}-${splitDate[1]
+                            .toString()
+                            .padStart(2, "0")}-${splitDate[2]
+                            .toString()
+                            .padStart(2, "0")}`;
+                    }
+
                     const eventDateObj = this.events[dateString];
                     if (eventDateObj) {
                         eventDateObj.push(eventData);
@@ -151,21 +205,38 @@ export class CalendarComponent implements OnInit, OnDestroy {
             });
     }
 
+    panelChange(event: { date: Date; mode: "month" | "year" }) {
+        this.selectChange(event.date);
+    }
+
     selectChange(event: Date) {
+        const dateObj = getCurrentLocalDate(event);
+        var dateString: string;
+        if (this.nzMode === "year") {
+            dateObj.day = undefined; // Set day to undefined for year mode
+            dateString = dateObj.toString();
+        } else {
+            dateString = dateObj.toString();
+        }
         this.router
-            .navigate([...this.baseUrlArr, event.toISOString().split("T")[0]], {
+            .navigate([...this.baseUrlArr, dateString], {
                 queryParamsHandling: "preserve",
             })
             .then(() => {
-                if (this.prevSelectedDate.getMonth() !== event.getMonth()) {
+                if (
+                    this.prevNzMode !== this.nzMode ||
+                    this.prevSelectedDate.getFullYear() !==
+                        event.getFullYear() ||
+                    this.prevSelectedDate.getMonth() !== event.getMonth()
+                ) {
                     this.runQuery();
                 }
                 this.prevSelectedDate = event;
+                this.prevNzMode = this.nzMode;
             });
     }
 
     ngOnDestroy(): void {
         this.authStateSubscription?.unsubscribe();
     }
-
 }
