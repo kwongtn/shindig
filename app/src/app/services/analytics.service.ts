cf33d51507a475b701668a8217bcf1239f7a13bb
Kwong Tung Nan <tungnan5636@gmail.com>
2025-10-10 00:02:44 +0800

    Resolve gtag issue
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/src/app/services/analytics.service.ts
+++ app/src/app/services/analytics.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 
 declare let gtag: Function;
 
@@ -6,23 +7,27 @@ declare let gtag: Function;
     providedIn: "root",
 })
 export class AnalyticsService {
-    constructor() {}
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
     trackPageview(path: string) {
-        try {
-            gtag("event", "page_view", {
-                page_path: path,
-            });
-        } catch (err) {
-            console.error("Error tracking pageview:", err);
+        if (isPlatformBrowser(this.platformId)) {
+            try {
+                gtag("event", "page_view", {
+                    page_path: path,
+                });
+            } catch (err) {
+                console.error("Error tracking pageview:", err);
+            }
         }
     }
 
     trackEvent(eventName: string, eventParams: {}) {
-        try {
-            gtag("event", eventName, eventParams);
-        } catch (err) {
-            console.error("Error tracking event:", err);
+        if (isPlatformBrowser(this.platformId)) {
+            try {
+                gtag("event", eventName, eventParams);
+            } catch (err) {
+                console.error("Error tracking event:", err);
+            }
         }
     }
 }
